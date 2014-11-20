9f8ec97991eeceb0181adf0473be33536c6868af
Laurent Humbert <laurent.humbert@laterooms.com>
2014-11-20 17:01:29 +0000

    introduced viewmodels

---
+++ MTG.MasterLister.ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using MTG.MasterLister.DataAccess;
+
+namespace MTG.MasterLister.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ObservableCollection<Card> _cards;
+
+        public ObservableCollection<Card> Cards
+        {
+            get { return _cards; } 
+            set 
+            { 
+                _cards = value;
+                OnPropertyChanged("Cards");
+            }
+        }
+
+        public MainViewModel()
+        {
+            
+        }
+
+    }
+}
